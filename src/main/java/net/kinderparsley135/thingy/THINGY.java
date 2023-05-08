78981b80f83f79bdfa9872d45bb880ea167636c9
Freddie Broxis <freddiebroxis@gmail.com>
2023-05-08 09:12:56 +0200

    Trying to upload files

---
+++ src/main/java/net/kinderparsley135/thingy/THINGY.java
@@ -0,0 +1,46 @@
+package net.kinderparsley135.thingy;
+
+import com.mojang.logging.LogUtils;
+import net.minecraftforge.common.MinecraftForge;
+import net.minecraftforge.eventbus.api.IEventBus;
+import net.minecraftforge.eventbus.api.SubscribeEvent;
+import net.minecraftforge.fml.common.Mod;
+import net.minecraftforge.fml.event.lifecycle.FMLClientSetupEvent;
+import net.minecraftforge.fml.event.lifecycle.FMLCommonSetupEvent;
+import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
+import org.slf4j.Logger;
+
+// The value here should match an entry in the META-INF/mods.toml file
+@Mod(THINGY.MOD_ID)
+public class THINGY
+{
+    // Define mod id in a common place for everything to reference
+    public static final String MOD_ID = "thingy";
+
+    private static final Logger LOGGER = LogUtils.getLogger();
+
+    public THINGY()
+    {
+        IEventBus modEventBus = FMLJavaModLoadingContext.get().getModEventBus();
+
+        modEventBus.addListener(this::commonSetup);
+
+        MinecraftForge.EVENT_BUS.register(this);
+    }
+
+    private void commonSetup(final FMLCommonSetupEvent event)
+    {
+
+    }
+
+
+    @Mod.EventBusSubscriber(modid = MOD_ID, bus = Mod.EventBusSubscriber.Bus.MOD)
+    public static class ClientModEvents
+    {
+        @SubscribeEvent
+        public static void onClientSetup(FMLClientSetupEvent event)
+        {
+
+        }
+    }
+}
